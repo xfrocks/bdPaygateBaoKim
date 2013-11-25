8abc7e0b9c57758975f0bd267b469f34b20ca27c
Dao Hoang Son <daohoangson@gmail.com>
2013-11-25 11:36:34 +0700

    added compatibility for [bd] Paygates v1.4

--- library/bdPaygateBaoKim/bdPaygate/Model/Processor.php
+++ library/bdPaygateBaoKim/bdPaygate/Model/Processor.php
@@ -2,12 +2,22 @@
 
 class bdPaygateBaoKim_bdPaygate_Model_Processor extends XFCP_bdPaygateBaoKim_bdPaygate_Model_Processor
 {
+	public function getCurrencies()
+	{
+		$currencies = parent::getCurrencies();
+
+		$currencies[bdPaygateBaoKim_Processor::CURRENCY_VND] = 'VND';
+
+		return $currencies;
+	}
+
 	public function getProcessorNames()
 	{
 		$names = parent::getProcessorNames();
-		
+
 		$names['baokim'] = 'bdPaygateBaoKim_Processor';
-		
+
 		return $names;
 	}
+
 }
